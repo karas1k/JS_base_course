af1270ccb44e73082752bbf4689d45ddee22f19a
karas1k <45075660+karas1k@users.noreply.github.com>
2020-03-09 19:12:19 +0300

    Home Work lesson_3, Ushakov M.V.

---
+++ 4.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(function (product) {
+    console.log(`Старая цена ${product.price}, c учетом скидки в 15% = ${product.price *= 0.85}`);
+});
---
+++ 7.js
@@ -0,0 +1,5 @@
+'use strict';
+
+for (let i = 0, x = "X"; i < 20; i++, x += "XX") {
+    console.log(i+1, x, x.length);
+}
